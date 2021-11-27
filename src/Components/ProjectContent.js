c6b7dfdfa5cce20d76de59e76d479a45ce7e0699
ICEACEs <icycaster41@gmail.com>
2021-11-27 10:38:39 +0700

    finish project section

--- src/Components/ProjectContent.js
+++ src/Components/ProjectContent.js
@@ -1,13 +1,44 @@
-import '../style/ProjectContent.css'
-
-function ProjectContent(){
-    return(
-        <div className="ProjectContent-container">
-            <h1 className="title center">
-                <span className="highlight">Projects</span>
-            </h1>
-        </div>
-    )
+import "../style/ProjectContent.css";
+import { Row, Col, Container } from "react-bootstrap";
+import project1 from "../img/Project/IoT.png";
+import project2 from "../img/Project/CT.PNG"
+function ProjectContent() {
+  const project = [
+    {
+      cover: project1,
+      date: "Publsihed on Oct 8, 2021",
+      title: "Iot Application",
+    },
+    {
+      cover: project2,
+      date: "Publsihed on Nov 26, 2021",
+      title: "Crypto-Table",
+    },
+  ];
+  return (
+    <div className="ProjectContent-container">
+      <h1 className="title center">
+        <span className="highlight">Projects</span>
+      </h1>
+      <Container>
+        <Row>
+          {project.map((item, index) => {
+            return (
+              <Col sm className="wrap-project">
+                <div className="card">
+                  <img src={item.cover} alt="Thumbnail" className="thumbnail" />
+                  <div className="content">
+                    <p className="date">{item.date}</p>
+                    <h3 className="project-title"><a className="link-to" href="">{item.title}</a></h3>
+                  </div>
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default ProjectContent;
