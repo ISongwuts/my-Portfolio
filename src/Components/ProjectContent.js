787272ace21b759681a7efbd58bcc2f2105e52da
ICEACEs <icycaster41@gmail.com>
2021-11-27 15:59:57 +0700

    add live demo in project content

--- src/Components/ProjectContent.js
+++ src/Components/ProjectContent.js
@@ -1,20 +1,28 @@
 import "../style/ProjectContent.css";
 import { Row, Col, Container } from "react-bootstrap";
 import project1 from "../img/Project/IoT.png";
-import project2 from "../img/Project/CT.PNG"
+import project2 from "../img/Project/CT.PNG";
+import project3 from "../img/Project/TodoList.PNG";
+
 function ProjectContent() {
   const project = [
     {
       cover: project1,
-      date: "Publsihed on Oct 8, 2021",
+      date: "Published on Oct 8, 2021",
       title: "Iot Application",
-      link: "https://github.com/ISongwuts/IOT-APP"
+      link: "https://github.com/ISongwuts/IOT-APP",
     },
     {
       cover: project2,
-      date: "Publsihed on Nov 26, 2021",
+      date: "Published on Nov 26, 2021",
       title: "Crypto-Table",
-      link: "https://github.com/ISongwuts/Crypto-Table"
+      link: "https://github.com/ISongwuts/Crypto-Table",
+    },
+    {
+      cover: project3,
+      date: "Published on Nov 27, 2021",
+      title: "Todo-List",
+      link: "https://github.com/ISongwuts/Crypto-Table",
     },
   ];
   return (
@@ -31,7 +39,18 @@ function ProjectContent() {
                   <img src={item.cover} alt="Thumbnail" className="thumbnail" />
                   <div className="content">
                     <p className="date">{item.date}</p>
-                    <h3 className="project-title"><a className="link-to" href={item.link}>{item.title}</a></h3>
+                    <Row>
+                      <Col>
+                        <h3 className="project-title">
+                          <a className="link-to" href={item.link}>
+                            {item.title}
+                          </a>
+                        </h3>
+                      </Col>
+                      <Col>
+                          <a className="liveDemo"href="/">Live Demo</a>
+                      </Col>
+                    </Row>
                   </div>
                 </div>
               </Col>
